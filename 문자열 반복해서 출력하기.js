81add74b1d3da3fae72a5e5419e3c360559b54c2
happy coding <lmw.hpc@gmail.com>
2025-10-23 07:54:18 +0900

    solved 문자열 반복해서 출력하기

---
+++ 문자열 반복해서 출력하기.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const inputSliced = input.split(' ');
+
+const num = Number(inputSliced[1])
+for(let i=0; i<num; i++)
+{
+    process.stdout.write(inputSliced[0]);
+}
